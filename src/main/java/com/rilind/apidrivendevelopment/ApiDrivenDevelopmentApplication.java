e54456aa908dbe52af81525b81bd63adc71f9d8c
Rilind Nuka <rilindnuka@gmail.com>
2022-03-18 13:36:32 +0100

    Initial project startup

---
+++ src/main/java/com/rilind/apidrivendevelopment/ApiDrivenDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.rilind.apidrivendevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDrivenDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDrivenDevelopmentApplication.class, args);
+    }
+
+}
